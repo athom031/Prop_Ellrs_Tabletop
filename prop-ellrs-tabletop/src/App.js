92621d17a9b6543f9a78350142a63c08f5f62b2b
Alex Thomas <thomaalex@meta.com>
2024-06-02 17:35:42 -0700

    added own starting boilerplate

--- prop-ellrs-tabletop/src/App.js
+++ prop-ellrs-tabletop/src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className='app-title'>
+        Prop Ellr's TableTop Journey
+      </h1>
+      <div className="app-content">
+        <div clsasName="board">
+
+        </div>
+        <div className="player-commands">
+
+        </div>
+      </div>
     </div>
   );
 }
